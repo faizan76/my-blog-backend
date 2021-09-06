fa5347bef8a13d1733a67d116018a21e3c925e8d
faizan76 <mfaizan76@gmail.com>
2021-09-06 23:10:08 +0500

    fake db

--- src/server.js
+++ src/server.js
@@ -5,6 +5,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0,
+    },
+    'learn-node': {
+        upvotes: 0,
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0,
+    },
+}
+
 app.get('hello', (req, res) => res.send('Hello!'));
 
 app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
