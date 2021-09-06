86e6265ddf7d69bfce8ebfab3cb1e34da269bfdd
faizan76 <mfaizan76@gmail.com>
2021-09-06 23:18:49 +0500

    post endpoint upvote articles

--- src/server.js
+++ src/server.js
@@ -17,6 +17,11 @@ const articlesInfo = {
     },
 }
 
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
 
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
+});
 
 app.listen(8000, () => console.log('listening on port 8000'));
