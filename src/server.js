1191e4a7cfe13d67a448c3629cc32720a501d835
faizan76 <mfaizan76@gmail.com>
2021-09-06 22:56:12 +0500

    post endpoint

--- src/server.js
+++ src/server.js
@@ -7,4 +7,6 @@ app.use(bodyParser.json());
 
 app.get('hello', (req, res) => res.send('Hello!'));
 
+app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`));
+
 app.listen(8000, () => console.log('listening on port 8000'));
