78cda31a94078350036c670a96419a9a0d5309fa
faizan76 <mfaizan76@gmail.com>
2021-09-06 23:28:42 +0500

    comments post endpoint

--- src/server.js
+++ src/server.js
@@ -27,4 +27,13 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
 });
 
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].comments.push({ username, text });
+
+    res.status(200).send(articlesInfo[articleName]);
+});
+
 app.listen(8000, () => console.log('listening on port 8000'));
