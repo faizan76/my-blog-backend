6c31714487ff32ac7753390503c9116d02687695
faizan76 <mfaizan76@gmail.com>
2021-09-07 07:26:00 +0500

    DRY:withDB(f) in get article endpoint

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const withDB = async (operations) => {
+const withDB = async (operations, res) => {
     try {
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
         const db = client.db('my-blog');
@@ -20,19 +20,11 @@ const withDB = async (operations) => {
 }
 
 app.get('/api/articles/:name', async (req, res) => {
-    try {
+    withDB(async (db) => {
         const articleName = req.params.name;
-
-        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-        const db = client.db('my-blog');
-
-        const articleInfo = await db.collection('articles').findOne({ name: articleName })
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
         res.status(200).json(articleInfo);
-
-        client.close();
-    } catch (error) {
-        res.status(500).json({ message: 'Error connecting to db', error });
-    }
+    }, res);
 });
 
 
